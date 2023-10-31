6a35da34cd87a3a260e6d1a7192f70300add165a
douirikenza <douirikenza8@gmail.com>
2023-10-31 15:11:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullName: "tayeb hanen",
+      bio: "As a mechanical engineer, I apply my passion for innovation and my strong expertise to design solutions that drive technology to new horizons.",
+      imgSrc: "https://storage.letudiant.fr/fme/job_card/188/tmp/ingenieur-mecanique-230411112324.jpg",
+      profession: "mechanical engineer",
+    },
+    shows: false,
+    intervalId: null,
+    secondsSinceMount: 0,
+  };
+
+  toggleShow = () => {
+    this.setState((prevState) => ({ shows: !prevState.shows }));
+  };
+
+  componentDidMount() {
+    const intervalId = setInterval(() => {
+      this.setState((prevState) => ({
+        secondsSinceMount: prevState.secondsSinceMount + 1,
+      }));
+    }, 1000);
+
+    this.setState({ intervalId });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  render() {
+    const { shows, person, secondsSinceMount } = this.state;
+
+    return (
+      <div className="App">
+        <button onClick={this.toggleShow}>Toggle Profile</button>
+        {shows && (
+          <div>
+            <img src={person.imgSrc} alt={person.fullName} />
+            <h1>{person.fullName}</h1>
+            <p>{person.bio}</p>
+            <p>Profession: {person.profession}</p>
+          </div>
+        )}
+        <p>Time since mount: {secondsSinceMount} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
